b71de766475e934dbfeb26e77151c88dcc1963cb
Victor.Dat <datlt@shipandplay.com>
2018-03-13 16:38:55 +0700

    update footer and header

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 				<div class="about_a">
 					<div class="row">
 						<div class="col-2 col-sm-2"><img src="<?php echo get_template_directory_uri() . '/images/about/about_3.png' ?>" alt=""></div>
-						<div class="col-10 col-sm-10"><b></b>
+						<div class="col-10 col-sm-10"><b>Thi công, Tư vấn giải pháp Quảng cáo Công nghệ Led</b>
 							<p></p></div>
 					</div>
 				</div>
@@ -14,7 +14,7 @@
 				<div class="about_b">
 					<div class="row">
 						<div class="col-2 col-sm-2"><img src="<?php echo get_template_directory_uri() . '/images/about/about_2.png' ?>" alt=""></div>
-						<div class="col-10 col-sm-10"><b></b>
+						<div class="col-10 col-sm-10"><b>Bảo hành từ 24 đến 60 tháng, ngay cả đối với đèn Led.</b>
 							<p></p></div>
 					</div>
 				</div>
@@ -23,7 +23,7 @@
 				<div class="about_c">
 					<div class="row">
 						<div class="col-2 col-sm-2"><img src="<?php echo get_template_directory_uri() . '/images/about/about_1.png' ?>" alt=""></div>
-						<div class="col-10 col-sm-10"><b></b>
+						<div class="col-10 col-sm-10">Giá thành luôn đảm bảo tốt nhất thị trường!<b></b>
 							<p></p></div>
 					</div>
 				</div>
@@ -32,7 +32,7 @@
 				<div class="about_d">
 					<div class="row">
 						<div class="col-2 col-sm-2"><img src="<?php echo get_template_directory_uri() . '/images/about/about_4.png' ?>" alt=""></div>
-						<div class="col-10 col-sm-10"><b>Liên hệ</b>
+						<div class="col-10 col-sm-10"><b>Liên hệ ngay</b>
 							<p><?php echo get_option( 'lst_phone' ); ?></p></div>
 					</div>
 				</div>
@@ -51,11 +51,11 @@
 
 				<div>Email: admin@bienhieuled.vn&nbsp;| Website: www.bienhieuled.vn</div>
 				<div class="bottom_mxh">
-					<a href="https://www.facebook.com" target="_blank"><i class="fa fa-facebook" aria-hidden="true" style=" color: #fff; background: #336699; "></i></a>
-					<a href="https://www.googleplus.com" target="_blank"><i class="fa fa-google-plus" aria-hidden="true" style=" color: #fff; background: #cc3333; "></i></a>
-					<a href="https://www.twitter.com" target="_blank"><i class="fa fa-twitter" aria-hidden="true" style=" color: #fff; background: #00aced; "></i></a>
-					<a href="https://www.linkedin.com" target="_blank"><i class="fa fa-linkedin" aria-hidden="true" style=" color: #fff; background: #0977b5; "></i></a>
-					<a href="https://www.youtube.com" target="_blank"><i class="fa fa-youtube" aria-hidden="true" style=" color: #fff; background: #cb3431; "></i></a>
+					<a href="https://www.facebook.com/bienhieuled.vn" target="_blank"><i class="fa fa-facebook" aria-hidden="true" style=" color: #fff; background: #336699; "></i></a>
+					<a href="https://plus.google.com/u/5/collection/kT60jB" target="_blank"><i class="fa fa-google-plus" aria-hidden="true" style=" color: #fff; background: #cc3333; "></i></a>
+					<a href="https://www.youtube.com/watch?v=UWVfcM18wZo" target="_blank"><i class="fa fa-twitter" aria-hidden="true" style=" color: #fff; background: #00aced; "></i></a>
+					<a href="https://www.facebook.com/bienhieuled.vn" target="_blank"><i class="fa fa-linkedin" aria-hidden="true" style=" color: #fff; background: #0977b5; "></i></a>
+					<a href="https://www.youtube.com/watch?v=UWVfcM18wZo" target="_blank"><i class="fa fa-youtube" aria-hidden="true" style=" color: #fff; background: #cb3431; "></i></a>
 				</div>
 			</div>
 		</div>
